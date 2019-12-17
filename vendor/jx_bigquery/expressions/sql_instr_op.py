2ab1257237f1ad1142d8d7a480c8865c36617278
Kyle Lahnakoski <kyle@lahnakoski.com>
2019-12-17 15:13:37 -0500

    add bigquery expressions

---
+++ vendor/jx_bigquery/expressions/sql_instr_op.py
@@ -0,0 +1,32 @@
+# encoding: utf-8
+#
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http:# mozilla.org/MPL/2.0/.
+#
+# Author: Kyle Lahnakoski (kyle@lahnakoski.com)
+#
+from __future__ import absolute_import, division, unicode_literals
+
+from jx_base.expressions import SqlInstrOp as SqlInstrOp_
+from jx_bigquery.expressions._utils import check
+from jx_bigquery.expressions.sql_instr_op import SqlInstrOp
+from mo_dots import wrap
+from pyLibrary.sql import sql_iso, sql_list
+
+
+class SqlInstrOp(SqlInstrOp_):
+    @check
+    def to_sql(self, schema, not_null=False, boolean=False):
+        value = self.value.to_sql(schema, not_null=True)[0].sql.s
+        find = self.find.to_sql(schema, not_null=True)[0].sql.s
+
+        return wrap(
+            [{"name": ".", "sql": {"n": "INSTR" + sql_iso(sql_list([value, find]))}}]
+        )
+
+    def partial_eval(self):
+        value = self.value.partial_eval()
+        find = self.find.partial_eval()
+        return SqlInstrOp([value, find])
